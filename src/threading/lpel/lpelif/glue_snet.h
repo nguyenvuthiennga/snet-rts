75635fd6eea542f9043878f3446b0b43e4092f9c
Nga Nguyen <nguyenvuthiennga@gmail.com>
2012-10-23 10:29:04 +0100

    set new record limit for different kinds of tasks

--- src/threading/lpel/lpelif/glue_snet.h
+++ src/threading/lpel/lpelif/glue_snet.h
@@ -1,11 +1,11 @@
 #ifndef _GLUE_SNET_H
 #define _GLUE_SNET_H
 
-#define BOX_REC_LIMIT 					10
-#define	PARALLEL_REC_LIMIT			20
-#define STAR_REC_LIMIT					10
-#define SPLIT_REC_LIMIT					20
-#define FILTER_REC_LIMIT				20
+#define BOX_REC_LIMIT 					20
+#define	PARALLEL_REC_LIMIT			40
+#define STAR_REC_LIMIT					20
+#define SPLIT_REC_LIMIT					40
+#define FILTER_REC_LIMIT				40
 #define OTHER_REC_LIMIT					-1
 
 
